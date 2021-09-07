d45435454e75205079f6e9a2d700bca3720d6531
Rezarick <chatti.oussama@outlook.com>
2021-09-07 22:26:38 +0200

    Moved MarshalledObject logic to ReflectionOnSeperateAppDomainHandler.

--- src/OpenSilver.Compiler/OtherHelpersAndHandlers/ApplicationEntryPointFinder.cs
+++ src/OpenSilver.Compiler/OtherHelpersAndHandlers/ApplicationEntryPointFinder.cs
@@ -22,6 +22,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Loader;
 using System.Text;
 using System.Threading.Tasks;
 using DotNetForHtml5.Compiler.Common;
@@ -30,69 +31,30 @@ namespace DotNetForHtml5.Compiler
 {
     internal static class ApplicationEntryPointFinder
     {
+        private static readonly AssemblyLoadContext context = new AssemblyLoadContext("Context", true);
         public static void GetFullNameOfClassThatInheritsFromApplication(string pathOfAssemblyThatContainsEntryPoint, out string applicationClassFullName, out string assemblyName, out string assemblyFullName)
         {
-            //-----------------
-            // Thanks to: http://www.c-sharpcorner.com/UploadFile/girish.nehte/how-to-unload-an-assembly-loaded-dynamically-using-reflection/
-            //-----------------
-
-            // Load the assembly in a new AppDomain mainly so that we are able to unload it, which is necessary when we want to delete all temporary files including this assembly.
-            AppDomainSetup setupInformation = AppDomain.CurrentDomain.SetupInformation;
-            AppDomain newAppDomain = AppDomain.CreateDomain("newAppDomain"/*, AppDomain.CurrentDomain.Evidence, setupInformation*/);
-
-            //Create an instance of the inspector class in the new domain:
-            //System.Runtime.Remoting.ObjectHandle obj = newAppDomain.CreateInstance(typeof(AssemblyInspectorOnOtherDomain).Assembly.FullName, typeof(AssemblyInspectorOnOtherDomain).FullName);
-            string pathOfThisVeryAssembly = PathsHelper.GetPathOfThisVeryAssembly();
-            System.Runtime.Remoting.ObjectHandle obj = newAppDomain.CreateInstanceFrom(pathOfThisVeryAssembly, typeof(AssemblyInspectorOnOtherDomain).FullName);
-
-            IAssemblyInspectorOnOtherDomain inspector = (IAssemblyInspectorOnOtherDomain)obj.Unwrap(); // As the object we are creating is from another appdomain hence we will get that object in wrapped format and hence in next step we have unwrappped it
-
-            // Call LoadAssembly method so that the assembly will be loaded into the new appdomain amd the object will also remain in new appdomain only:
-            inspector.LoadAssembly(pathOfAssemblyThatContainsEntryPoint);
+            var assembly = context.LoadFromAssemblyPath(pathOfAssemblyThatContainsEntryPoint);
 
             // Call the method that finds the type that inherits from Application:
-            applicationClassFullName = inspector.FindApplicationClassFullName();
+            applicationClassFullName = FindApplicationClassFullName(assembly);
 
             // Get the assembly name and full name too:
-            assemblyName = inspector.GetAssemblyName();
-            assemblyFullName = inspector.GetAssemblyFullName();
+            assemblyName = assembly.GetName().Name;
+            assemblyFullName = assembly.FullName;
 
             // Unload the assembly (so that we can later delete it if necessary):
-            AppDomain.Unload(newAppDomain);
-            GC.Collect(); // Collects all unused memory
-            GC.WaitForPendingFinalizers(); // Waits until GC has finished its work
-            GC.Collect();
+            context.Unload();
         }
 
 
-        public class AssemblyInspectorOnOtherDomain : MarshalByRefObject, IAssemblyInspectorOnOtherDomain
+        public static string FindApplicationClassFullName(Assembly assembly)
         {
-            private Assembly _assembly;
-            System.Type MyType = null;
-            object inst = null;
-            public override object InitializeLifetimeService()
-            {
-                return null;
-            }
-            public void LoadAssembly(string path)
-            {
-                _assembly = Assembly.Load(AssemblyName.GetAssemblyName(path));
-            }
-            public string GetAssemblyName()
-            {
-                return _assembly.GetName().Name;
-            }
-            public string GetAssemblyFullName()
-            {
-                return _assembly.FullName;
-            }
-            public string FindApplicationClassFullName()
-            {
 #if BRIDGE || CSHTML5BLAZOR
-                throw new NotSupportedException();
+            throw new NotSupportedException();
 #else
                 // Get the base "Application" type:
-#if SILVERLIGHTCOMPATIBLEVERSION 
+#if SILVERLIGHTCOMPATIBLEVERSION
                 Type baseApplicationType = typeof(DotNetForHtml5Core::System.Windows.Application);
 #else
                 Type baseApplicationType = typeof(DotNetForHtml5Core::Windows.UI.Xaml.Application);
@@ -115,45 +77,6 @@ namespace DotNetForHtml5.Compiler
 
                 return applicationType.FullName;
 #endif
-            }
         }
-
-
-        //class AssemblyReflectionOnlyLoaderIncludingDependencies
-        //{
-        //    // This class is used to load an assembly for reflection only, including all its dependencies.
-
-        //    Assembly _assembly;
-        //    string _assemblyPath;
-
-        //    public AssemblyReflectionOnlyLoaderIncludingDependencies(string assemblyPath)
-        //    {
-        //        AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve -= ReflectionOnlyAssemblyResolve;
-        //        AppDomain.CurrentDomain.ReflectionOnlyAssemblyResolve += ReflectionOnlyAssemblyResolve;
-        //        _assemblyPath = assemblyPath;
-        //        _assembly = Assembly.ReflectionOnlyLoadFrom(_assemblyPath);
-        //    }
-
-        //    public Assembly Assembly
-        //    {
-        //        get
-        //        {
-        //            return _assembly;
-        //        }
-        //    }
-
-        //    private Assembly ReflectionOnlyAssemblyResolve(object sender, ResolveEventArgs args)
-        //    {
-        //        AssemblyName name = new AssemblyName(args.Name);
-        //        String asmToCheck = Path.GetDirectoryName(_assemblyPath) + "\\" + name.Name + ".dll";
-
-        //        if (File.Exists(asmToCheck))
-        //        {
-        //            return Assembly.ReflectionOnlyLoadFrom(asmToCheck);
-        //        }
-
-        //        return Assembly.ReflectionOnlyLoad(args.Name);
-        //    }
-        //}
     }
 }
